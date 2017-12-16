ab282c6bbfd781ce54ec0af0f9179cc37b395c5a
Brett Hays <brett.g.hays.coding@gmail.com>
2017-12-16 08:50:20 -0700

    latest commit

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -18,7 +18,7 @@ export default class FilterObject extends Component {
 
     filterArray(y){
         let unfiltered = this.state.unFilteredArray
-        let filtered = unfiltered.filter((x,y) => x[y])
+        let filtered = unfiltered.filter(x => x['eyeColor'])
         console.log(filtered)
     }
 
